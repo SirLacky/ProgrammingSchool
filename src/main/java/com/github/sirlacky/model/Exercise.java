75d1eb7bdff23808330eda35f7a0d6027c3b093c
Łukasz Koziara <SirLacky@users.noreply.github.com>
2019-06-09 18:24:12 +0200

    Added Exercise Class

---
+++ src/main/java/com/github/sirlacky/model/Exercise.java
@@ -0,0 +1,43 @@
+package com.github.sirlacky.model;
+
+public class Exercise {
+
+    private Long id;
+    private String title;
+    private String description;
+
+    public Exercise(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+
+
+    public Exercise() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/com/github/sirlacky/model/Solution.java
+++ src/main/java/com/github/sirlacky/model/Solution.java
@@ -27,7 +27,7 @@ public class Solution {
         return id;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
