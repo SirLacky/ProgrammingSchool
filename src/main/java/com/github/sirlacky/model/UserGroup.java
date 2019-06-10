9a02e6d710029413405ba4cd6c6caa8032dad0ef
Łukasz Koziara <SirLacky@users.noreply.github.com>
2019-06-10 22:48:16 +0200

    Added UserGroup class

---
+++ src/main/java/com/github/sirlacky/model/UserGroup.java
@@ -0,0 +1,32 @@
+package com.github.sirlacky.model;
+
+public class UserGroup {
+
+    private Long id;
+    private String name;
+
+    
+    public UserGroup() {
+    }
+
+    public UserGroup(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
