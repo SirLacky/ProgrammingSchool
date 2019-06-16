30bdd052ad0aee5c09cdc5043efb203163b9cce2
Łukasz Koziara <SirLacky@users.noreply.github.com>
2019-06-16 15:14:12 +0200

    Added console administration for Admins

--- src/main/java/com/github/sirlacky/Main.java
+++ src/main/java/com/github/sirlacky/Main.java
@@ -10,21 +10,80 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+        Scanner scanner = new Scanner(System.in);
         UserDao userDao = new UserDao();
-        Solution solution = new Solution();
-        SolutionDao solutionDao = new SolutionDao();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        ArrayList<Solution> odp = new ArrayList<>();
-        odp = solutionDao.findAll();
+        System.out.println("Witaj w programie! Wybierz interesujący cię panel: ");
+        System.out.println("Administrator [1]");
+        System.out.println("User [2]");
+        int nextInt = scanner.nextInt();
 
-        System.out.println(odp.size());
+        if (nextInt == 1) {
+            System.out.println("Witaj Adminie. Oto lista użytkowników: ");
+            List<User> users = userDao.findAll();
+            System.out.println(users.toString());
 
+            System.out.println("Wpisz komende: add / edit / delete / quit");
+            Scanner scan = new Scanner(System.in);
+            String adminCommend = scan.nextLine();
+
+            if (adminCommend.equals("add")) {
+                Scanner scan1 = new Scanner(System.in);
+                User user = new User();
+                System.out.println("Podaj nick użytkownika: ");
+                String nick = scan.next();
+                user.setUserName(nick);
+                System.out.println("Podaj hasło użytkownika: ");
+                String password = scan.next();
+                user.setPassword(password);
+                System.out.println("Podaj maila użytkownika: ");
+                String email = scan.next();
+                user.setEmail(email);
+                userDao.create(user);
+
+            } else if (adminCommend.equals("edit")) {
+                Scanner scan1 = new Scanner(System.in);
+                System.out.println("Podaj id użytkownika do edycji: ");
+                int userId = scan.nextInt();
+                User user = new User();
+                user = userDao.readById(userId);
+                System.out.println("Edycja użytkownika: " + user.toString());
+                System.out.println("Podaj nowy nick użytkownika: ");
+                String nick = scan.next();
+                user.setUserName(nick);
+                System.out.println("Podaj nowe hasło użytkownika: ");
+                String password = scan.next();
+                user.setPassword(password);
+                System.out.println("Podaj nowego maila użytkownika: ");
+                String email = scan.next();
+                user.setEmail(email);
+                userDao.update(user);
+                System.out.println("Zedytowano dane użytkownika: " + user.toString());
+
+            } else if (adminCommend.equals("delete")) {
+
+                Scanner scan2 = new Scanner(System.in);
+                System.out.println("Podaj id użytkownika do usunięcia: ");
+                int userId = scan.nextInt();
+                userDao.delete(userId);
+                System.out.println("Usunięto użytkownika o id: "+userId);
+                
+            } else {
+                System.out.println("Koniec panelu Administracyjnego");
+            }
+
+
+        } else if (nextInt == 2) {
+            System.out.println("Witaj Userze");
+        } else {
+            System.out.println("Wprowadzono nieproprawne dane");
+        }
     }
 }
